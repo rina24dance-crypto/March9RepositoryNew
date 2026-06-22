3e69abc21f4b8b75cfed8e53aca78d1d5c58baa2
rina24dance-crypto <rina24dance@gmail.com>
2026-06-22 17:27:18 +0600

    DOM homework

---
+++ DOM Homework/DOM Homework.js
@@ -0,0 +1,32 @@
+//  let paragraphs = document.getElementsByClassName("message");
+// for (let i = 0; i < paragraphs.length; i++) {
+//     paragraphs[i].textContent = "Обновленный текст"; 
+
+// }
+
+
+// Task 2 (wrong)
+
+// let box = document.getElementsByClassName(".box1");
+// for (let i = 0; i <box.length; i++) {
+//     box.style.color("blue")
+// }
+
+
+// (right)
+
+
+// let boxes = document.getElementsByClassName("box");    // Это же есть и в html но уже с тэгом script, можно и там и здесь использовать
+
+//         for (let i = 0; i < boxes.length; i++) {
+//             boxes[i].style.backgroundColor = "green";
+//         }
+
+
+
+// Task 3
+
+// const firstText = document.querySelector('.text')
+// firstText.textContent = "Wassup"
+
+
